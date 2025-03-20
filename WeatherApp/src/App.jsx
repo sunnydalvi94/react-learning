26106c533b0a34608171f642a68aa768f2d81efa
sunnydalvi94 <sunnydalvi94@gmail.com>
2025-03-20 09:20:52 +0530

    WeatherApp

--- WeatherApp/src/App.jsx
+++ WeatherApp/src/App.jsx
@@ -1,52 +1,94 @@
 import { useState } from "react";
-
-const App = () => {
-  const [city, setCity] = useState("");
-  const [weather, setWeather] = useState(null);
-  const API_KEY = import.meta.env.VITE_API_KEY;
-
-  const fetchWeather = async () => {
-    if (!city) return;
-
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
-
+import { useEffect } from "react";
+import "./App.css";
+function App() {
+  const [city, setCity] = useState("London");
+  const [text, setText] = useState("");
+  const [weatherInfo, setWeatherInfo] = useState([]);
+  let API_KEY = import.meta.env.VITE_API_KEY;
+  const WeatherInfo = async () => {
     try {
-      const response = await fetch(url);
+      // const response = await fetch(
+      //   `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
+      // );
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${API_KEY}`
+      );
+
       const data = await response.json();
-
-      if (data.cod !== 200) {
-        throw new Error(data.message);
-      }
-
-      setWeather(data);
+      setWeatherInfo(data);
+      console.log(data);
     } catch (error) {
-      alert(error.message);
-      setWeather(null);
+      console.error(error);
     }
   };
+  // WeatherInfo();
+  useEffect(() => {
+    WeatherInfo();
+  }, [city]);
 
+  function handleInput(value) {
+    setText(value);
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    setCity(text);
+  }
   return (
-    <div>
-      <h2>Weather App</h2>
-      <input
-        type="text"
-        placeholder="Enter city name"
-        value={city}
-        onChange={(e) => setCity(e.target.value)}
-      />
-      <button onClick={fetchWeather}>Get Weather</button>
-
-      {weather && (
-        <div>
-          <h3>{weather.name}, {weather.sys.country}</h3>
-          <p>🌡️ Temperature: {(weather.main.temp - 273.15).toFixed(2)}°C</p>
-          <p>💨 Wind Speed: {weather.wind.speed} m/s</p>
-          <p>🌦️ Weather: {weather.weather[0].description}</p>
-          <p>💧 Humidity: {weather.main.humidity}%</p>
+    <>
+      <div className="weather-main">
+        <form
+          className="search-box"
+          action=""
+          onSubmit={(e) => handleSubmit(e)}
+        >
+          <input type="text" onChange={(e) => handleInput(e.target.value)} />
+          <button> 🔍</button>
+        </form>
+        <div className="info-section-one">
+          <img
+            src={`https://openweathermap.org/img/wn/${weatherInfo?.weather?.[0]?.icon}@2x.png`}
+            alt=""
+          />
+          <h1>{Math.floor(weatherInfo?.list?.[0]?.main?.temp)} °C</h1>
+          <h3>{weatherInfo?.city?.name}</h3>
         </div>
-      )}
-    </div>
+        <div className="info-section-two">
+          <p>
+            {" "}
+            {weatherInfo?.main?.humidity}%
+            <br />
+            <span>💧 Humidity </span>
+          </p>
+          <p>
+            {weatherInfo?.wind?.speed} km/hr <br />
+            <span>💨 Wind Speed </span>
+          </p>
+        </div>
+        <div className="forcast">
+          {weatherInfo?.list?.map(
+            (item, index) =>
+              index < 4 && (
+                <div key={index}>
+                  <div className="info-section-two">
+                    <p>
+                      {" "}
+                      {item?.main?.humidity}%
+                      <br />
+                      <span>💧 Humidity </span>
+                    </p>
+                    <p>
+                      {item?.wind?.speed} km/hr <br />
+                      <span>💨 Wind Speed </span>
+                    </p>
+                  </div>
+                </div>
+              )
+          )}
+        </div>
+      </div>
+    </>
   );
-};
+}
 
 export default App;
